bf083b828e0955c83a40665860ca3e3311962069
Shayan Fallahian <shayanf@kth.se>
2017-11-10 21:37:46 +0100

    initial commit

---
+++ src/server/ClientHandler.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2017 Shayan Fallahian shayanf@kth.se
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package server;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class ClientHandler extends Thread {
+
+    private Socket client;
+    private Scanner input;
+    private PrintWriter output;
+
+    public ClientHandler(Socket socket) {
+        client = socket;
+
+        try {
+            input = new Scanner(client.getInputStream());
+            output = new PrintWriter(client.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        String received;
+
+        do {
+            received = input.nextLine();
+            output.println("ECHO: " + received);
+        } while (!received.equals("QUIT"));
+
+        try {
+            if (client != null) {
+                System.out.println("Closing down connection.");
+                client.close();
+            }
+        } catch (IOException e) {
+            System.out.println("Unable to disconnect!");
+            e.printStackTrace();
+        }
+    }
+}
